0123debd121d7cfd06b95fd9c970e68cf48e6613
Levi de Oliveira Filho <162984256+Lewis090@users.noreply.github.com>
2026-05-08 15:28:58 -0300

    esqueci a ,

--- src/main/java/com/easy/easyapi/config/CorsConfig.java
+++ src/main/java/com/easy/easyapi/config/CorsConfig.java
@@ -20,7 +20,7 @@ public class CorsConfig {
                         "http://127.0.0.1:5173",
                         "http://localhost:5173",
                         "https://ease-frontend-main*.vercel.app", // Aceita qualquer deploy da vercel deste projeto
-                        "https://vercel.app"
+                        "https://vercel.app",
                         "https://ease-frontend-tau.vercel.app"// Coloque aqui a URL exata que a Vercel te deu
                     )
                     .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
